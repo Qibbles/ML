46fdd3405da87f32bd0245d42dc80ca0c3b998e2
Qibbles <coffeecup@gmail.com>
2021-09-14 11:47:18 +0800

    complete draw.js

--- Simple Perceptron Example by Artificial Intelligence/draw.js
+++ Simple Perceptron Example by Artificial Intelligence/draw.js
@@ -42,3 +42,50 @@ function setup() {
         };
     }
 }
+
+function draw() {
+    background(0);
+    
+    // Draw the line
+    strokeWeight(1);
+    stroke(255);
+    let x1 = map(xmin, xmin, xmax, 0, width);
+    let y1 = map(f(xmin), ymin, ymax, height, 0);
+    let x2 = map(xmax, xmin, xmax, 0, width);
+    let y2 = map(f(xmax), ymin, ymax, geight, 0);
+    line(x1, y1, x2, y2);
+    
+    // Draw the line based on the current weights
+    // Formula is weights[0] * x + weights[1] * y + weights[2] = 0
+    stroke(255);
+    strokeWeight(2);
+    let weights = ptron.getWeights();
+    x1 = xmin;
+    y1 = (-weights[2] - weights[0] * x1) / weights[1];
+    x2 = xmax;
+    y2 = (-weights[2] - weights[0] * x2) / weights[1];
+    
+    x1 = map(x1, xmin, xmax, 0, width);
+    y1 = map(y1, ymin, ymax, height, 0);
+    x2 = map(x2, xmin, xmax, 0, width);
+    y2 = map(y2,ymin, ymax, height, 0);
+    line(x1, y1, x2, y2);
+    
+    // Train the perceptron with one "training" point at a time
+    ptron.train(training[count].input, training[count].output);
+    count = (count + 1) % training.length;
+    
+    // Draw all the points based on what the perceptron would "guess"
+    // Does not use the "known" correct answer
+    for (let i = 0; i < count; i++) {
+        stroke(255);
+        strokeWeight(1);
+        fill(255);
+        let guess = ptron.feedforward(training[i].input);
+        if (guess > 0) noFill();
+        
+        let x = map(training[i].input[0], xmin, xmax, 0, width);
+        let y = map(training[i].input[1], ymin, ymax, height, 0);
+        ellipse(x, y, 8, 8);
+    }
+}
