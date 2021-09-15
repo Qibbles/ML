4c9678c4042392caf8e7a7964d47b95d389fb17f
Qibbles <coffeecup@gmail.com>
2021-09-15 11:51:58 +0800

    complete

--- Simple Perceptron Example by Artificial Intelligence/learning.js
+++ Simple Perceptron Example by Artificial Intelligence/learning.js
@@ -5,3 +5,45 @@ class Perceptron {
     this.weights = new Array(n);
     // Start with random weights
     for (let i = 0; i < this.weights.length; i++) {
+      this.weight[i] = random(-1,1);
+    }
+    this.c = c; // learning rate/constant
+  }
+  
+  // FUnction to train the perception
+  // Weights are adjusted based on "desired" answer
+  train(inputs, desired) {
+    // Guess the result
+    let guess = this.feedforward(inputs);
+    // Compute the factor for changing the weight based on the error
+    // Error = desired output - guessed output
+    // Note this can only be 0, -2, or 2
+    // Multiply by learning constant
+    let error = desired - guess;
+    // Adjust weights based on weightChange * input
+    for (let i = 0; i < this.weights.length; i++) {
+      this.weight[i] += this.c * error * inputs[i];
+    }
+  }
+  
+  // Guesse -1 or 1 based on input values
+  feedforward(inputs) {
+    // Sum all values
+    let sum = 0;
+    for (let i = 0; i < this.weights.legth; i++) {
+      sum += inputs[i] * this.weights[i];
+    }
+    // Result is sign of the sum, -1 or 1
+    return this.activate(sum);
+  }
+  
+  activate(sum) {
+    if (sum > 0) return 1;
+    else return -1;
+  }
+  
+  // Return weights
+  getWeights() {
+    return this.weights;
+  }
+}
